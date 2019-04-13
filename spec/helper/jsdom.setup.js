c9416112700397af5988efc6ee1146aa1ce0b3c3
Akigaze <1175520196@qq.com>
2019-04-13 16:28:14 +0800

    change project indent

--- spec/helper/jsdom.setup.js
+++ spec/helper/jsdom.setup.js
@@ -3,4 +3,4 @@ import {JSDOM} from "jsdom";
 const dom = new JSDOM("<html><body></body></html>");
 global.window = dom.window;
 global.document = dom.window.document;
-global.navigator = dom.window.navigator
+global.navigator = dom.window.navigator;
--- src/app.js
+++ src/app.js
@@ -4,15 +4,16 @@ import classNames from "classnames";
 import appStyle from "./style/app.css";
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render(){
-        const titleClassName = classNames(appStyle["big-red-title"]);
-        return (
-            <div>
-                <h1 className={titleClassName}>Hello React!</h1>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const titleClassName = classNames(appStyle["big-red-title"]);
+    return (
+      <div>
+        <h1 className={titleClassName}>Hello React!</h1>
+      </div>
+    )
+  }
 }
